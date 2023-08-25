d1fe813a83ab856e0f19e26083b8afd5b836efd0
sasha509 <abc509@yandex.ru>
2023-08-25 15:54:38 +0300

    fix: исправить валидацию полей профиля

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popupProfile = document.querySelector('.popup_type_about');
 const buttonClosePopupProfile = popupProfile.querySelector('.popup__button-close');
-const form = document.querySelector('.form');
+const formProfile = popupProfile.querySelector('.form');
+// const inputsProfile = popupProfile.querySelectorAll('.form__input')
 const nameInput = popupProfile.querySelector('.form__input_type_name');
 const jobInput = popupProfile.querySelector('.form__input_type_about');
 const profileTitle = document.querySelector('.profile__title');
@@ -14,16 +15,12 @@ const profileSubtitle = document.querySelector('.profile__subtitle');
 function openPopup(popup) {
   popup.classList.add('popup_opened')
   // закрытие попапов по Esc
-  document.addEventListener('keydown', function(evt) {
-    if (evt.key === 'Escape') {
-      closePopup(popup)
-  }
-
-  });
+  document.addEventListener('keydown', closeOnEsc);
 }
 // // универсальная Ф закрытия попапов
 function closePopup(popup) {
   popup.classList.remove('popup_opened')
+  document.removeEventListener('keydown', closeOnEsc);
 }
 
 // edit Профиль
@@ -31,28 +28,43 @@ function addProfile() {
   openPopup(popupProfile);
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
+
+  resetInputErrors(popupProfile, configForm)
 }
+
+function resetInputErrors(formElement, config) {
+  const formInputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const errorProfile = Array.from(formElement.querySelectorAll('.form__error'));
+  formInputList.forEach(inputElement => {
+    inputElement.classList.remove(config.inputErrorClass)
+  });
+  errorProfile.forEach(errorElement => {
+    errorElement.textContent = ''
+  });
+}
+
 
 editButton.addEventListener('click', addProfile);
 
 // Редактирование попапа Профиль
-function handleFormSubmit (evt) {
+function handleProfileSubmit (evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
   closePopup(popupProfile)
 }
+
 buttonClosePopupProfile.addEventListener('click', () =>
 {closePopup(popupProfile)});
 popupProfile.addEventListener('click', (evt) => {closeOnOverlay(evt, popupProfile)});
 
-form.addEventListener('submit', handleFormSubmit);
+formProfile.addEventListener('submit', handleProfileSubmit);
 
 // переменые для Ф открыть/закрыть Новое место
 const addButton = document.querySelector('.profile__add-button');
 const popupNewElement = document.querySelector('.popup_type_new-element');
 const closeBtnNewElement = popupNewElement.querySelector('.popup__button-close');
-
+const submitButtonNewElement = popupNewElement.querySelector('.form__button-save')
 addButton.addEventListener('click', () => {openPopup(popupNewElement)});
 closeBtnNewElement.addEventListener('click', () => {closePopup(popupNewElement)});
 // ФУНКЦИЯ ЗАКРЫТИЯ ПО ОВЕРЛЕЮ
@@ -61,8 +73,14 @@ function closeOnOverlay(evt, openedPopup) {
     closePopup(openedPopup)
 }
 };
-
-
+// ФУНКЦИЯ ЗАКРЫТИЯ ПО Esc
+function closeOnEsc(evt) {
+  if (evt.key === 'Escape') {
+    openedPopupClass = document.querySelector('.popup_opened');
+    closePopup(openedPopupClass)
+}
+};
+// обработчик для закрытия попапа Новое место  по overlay
 popupNewElement.addEventListener('click', (evt) => {closeOnOverlay(evt, popupNewElement)});
 
 // Добавляем попап Новое место
@@ -102,6 +120,7 @@ const  createCard = (nameValue, linkValue)  => {
 // слушаетель на img и вызов ф открытия попапа viewPhoto с данными из массива
 cardImg.addEventListener('click', () => {
   openView(linkValue, nameValue)
+  viewContainer.addEventListener('click', (evt) => {closeOnOverlay(evt, viewContainer)});
 })
 
 // функция открытия попапа viewPhoto,
@@ -109,7 +128,7 @@ function openView(img, title) {
   viewPhoto.src = img;
   viewTitle.textContent = title;
   openPopup(viewContainer);
-  viewContainer.addEventListener('click', (evt) => {closeOnOverlay(evt, viewContainer)});
+
 }
 
   return cardElement;
@@ -139,75 +158,8 @@ function handleAddCards (evt) {
   renderCard(nameInputPlace.value, linkInputLink.value);
   closePopup(popupNewElement)
   formNewElement.reset()
+  toggleButtonState(submitButtonNewElement, popupNewElement.checkValidity, configForm);
+
 }
 formNewElement.addEventListener('submit', handleAddCards);
-
-// В А Л И Д А Ц И Я
-// Ф отображения сообщения об ошибке validationMessage и добоваление соотв класса
-function showError(inputElement, errorElement, config) {
-  errorElement.textContent = inputElement.validationMessage;
-  inputElement.classList.add(config.inputErrorClass);
-}
-// Ф скрытия сообщения об ошибке validationMessage и добоваление соотв класса
-function hideError(inputElement, errorElement, config) {
-  inputElement.classList.remove(config.inputErrorClass);
-  errorElement.textContent = inputElement.validationMessage;
-}
-// При наступлении события ввода в инпут проверяем его валидность и вызываем функции закрытия/отображения ошибки
-function checkInputValidity(inputElement, formElement, config) {
-  const isInputValid = inputElement.validity.valid;
-  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
-  if (isInputValid) {
-    hideError(inputElement, errorElement, config)
-  } else {
-    showError(inputElement, errorElement, config)
-  }
-}
-// Переключатель кнопки Сохранить и добавление соотв класса
-function toggleButtonState(buttonElement, isActive, config) {
-if (isActive) {
-  buttonElement.disabled = false;
-  buttonElement.classList.remove(config.finactiveButtonClass);
-} else {
-  buttonElement.disabled = 'disabled';
-  buttonElement.classList.add(config.finactiveButtonClass);
-}
-}
-// Вешаем обработчик события submit на каждую форму в переборе
-// Внутри каждой формы ищем инпуты
-// Перебираем список инпутов конкретной формы и вашеам на кадый инпутт обработчик события input
-
-function setEventListener(formElement, config) {
-  const inputList = formElement.querySelectorAll(config.inputSelector);
-  const submitButtonElement = formElement.querySelector(config.submitButtonSelector);
-  toggleButtonState(submitButtonElement, formElement.checkValidity(), config);
-  [...inputList].forEach(function(inputElement) {
-    inputElement.addEventListener('input', function(){
-      toggleButtonState(submitButtonElement, formElement.checkValidity(), config);
-      checkInputValidity(inputElement, formElement,config)
-    })
-
-    })
-  formElement.addEventListener('submit', function(evt){
-    evt.preventDefault();
-  })
-}
-// 1. Находим все формы и перебираем их
-// параметры, сюда передаем Аргументы формы-объекта
-function enableValidation(config) {
-  const formList = document.querySelectorAll(config.formSelector);
-  [...formList].forEach(function(formElement){
-    setEventListener(formElement, config)
-    })
-};
-// Аргументы формы объект
-const configForm = {
-  formSelector: ".form",
-  inputSelector: ".form__input",
-  submitButtonSelector: ".form__button-save",
-  finactiveButtonClass: "form__button-save_disabled",
-  inputErrorClass: "form__input_type_error"
-}
-
-enableValidation(configForm);
 
